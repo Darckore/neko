4c9368a0c848a85b3a5401f8796cb255723d0177
darckore <darckore@gmail.com>
2022-04-29 13:36:23 +0300

    documented some managers

--- neko/include/managers/app_host.hpp
+++ neko/include/managers/app_host.hpp
@@ -1,13 +1,25 @@
+//
+// Application host
+//
+
 #pragma once
 #include "core/singleton_base.hpp"
 
 namespace neko
 {
+  //
+  // Abstract class representing the application host
+  // Platform-dependent inherited windows implement the details
+  // Used as the main render target, may also produce input events
+  //
   class app_host : private singleton<app_host>
   {
   public:
     using size_type = std::int32_t;
 
+    //
+    // Host width and height
+    //
     struct dimensions
     {
       bool operator==(const dimensions&) const noexcept = default;
@@ -24,7 +36,15 @@ namespace neko
     app_host() noexcept = default;
 
   public:
+    //
+    // Called every frame
+    // Returns false is the application needs to close
+    //
     virtual bool update() noexcept = 0;
+    
+    //
+    // Returns host width and height
+    //
     virtual dimensions size() const noexcept = 0;
   };
 }
