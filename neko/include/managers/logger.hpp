56385c21c49d842d215c8729f6c7709128daab07
darckore <darckore@gmail.com>
2022-04-29 14:06:18 +0300

    documented rest of the implemented systems

--- neko/include/managers/logger.hpp
+++ neko/include/managers/logger.hpp
@@ -1,10 +1,21 @@
+//
+// Engine logger
+//
+
 #pragma once
 
 namespace neko
 {
+  //
+  // Logger for the engine
+  // Systems and user code can use it to post information to the application log
+  //
   class logger final
   {
   public:
+    //
+    // Loggind level
+    //
     enum class level : std::uint8_t
     {
       off,
@@ -24,6 +35,9 @@ namespace neko
     CLASS_SPECIALS_NONE(logger);
 
   private:
+    //
+    // Generates a time stamp and message type indicator
+    //
     static void prologue(level lvl)
     {
       constexpr std::array severities {
@@ -45,6 +59,10 @@ namespace neko
                      zt);
     }
 
+    //
+    // Posts a message of the specified level
+    // The fmt parameter is a format string (see std::format)
+    //
     template <typename ...Args>
     static void message(level lvl, fmt_type fmt, Args&& ...args) noexcept
     {
@@ -82,11 +100,18 @@ namespace neko
     }
 
   public:
+    //
+    // Checks the log file state
+    //
     static bool good() noexcept
     {
       return static_cast<bool>(m_file);
     }
 
+    //
+    // Initialises the logger
+    // Creates the underlying buffer and opens the log file for writing
+    //
     static void init() noexcept
     {
       constexpr auto initialSize = 256ull;
@@ -96,6 +121,9 @@ namespace neko
       NEK_ASSERT(good());
     }
 
+    //
+    // Writes buffer contents to the file and clears the buffer
+    //
     static void dump() noexcept
     {
       NEK_ASSERT(static_cast<bool>(m_file));
@@ -103,12 +131,20 @@ namespace neko
       m_stream = {};
     }
 
+    //
+    // Writes data to the file and closes it
+    //
     static void shutdown() noexcept
     {
       dump();
       m_file.close();
     }
 
+    //
+    // Sets the logging level
+    // Returns the previously set one
+    // Applications can use this to override the level temporarily
+    //
     static level set_severity_level(level lvl) noexcept
     {
       const auto prev = m_lvl;
@@ -116,14 +152,25 @@ namespace neko
       return prev;
     }
 
+    //
+    // Assigns a new file to write to
+    //
     static void assign_file(file_name fname) noexcept
     {
+      if (good())
+      {
+        dump();
+      }
+
       m_fname = std::move(fname);
       m_file.open(m_fname);
       NEK_ASSERT(good());
     }
 
   #ifndef NDEBUG
+    //
+    // Debug trace message of the lowest level possible
+    //
     template <typename ...Args>
     static void trace(fmt_type fmt, Args&& ...args) noexcept
     {
@@ -131,24 +178,40 @@ namespace neko
     }
   #endif
 
+    //
+    // A note (informational message)
+    // Disabled in release builds by default
+    //
     template <typename ...Args>
     static void note(fmt_type fmt, Args&& ...args) noexcept
     {
       message(msg, fmt, std::forward<Args>(args)...);
     }
 
+    //
+    // A warning (something to worry about, but the engine can continue running)
+    // Disabled in release builds by default
+    //
     template <typename ...Args>
     static void warning(fmt_type fmt, Args&& ...args) noexcept
     {
       message(warn, fmt, std::forward<Args>(args)...);
     }
 
+    //
+    // An error (fatal condition which leads to engine shutdown)
+    // Always enabled
+    //
     template <typename ...Args>
     static void error(fmt_type fmt, Args&& ...args) noexcept
     {
       message(err, fmt, std::forward<Args>(args)...);
     }
 
+    //
+    // An abnormal termination message
+    // Posted when normal logging is impossible (e.g. in an out of memory situation)
+    //
     static void abnormal(std::string_view msg) noexcept
     {
       NEK_ASSERT(good());
@@ -157,12 +220,29 @@ namespace neko
     }
 
   private:
+    //
+    // Log file name
+    //
     inline static file_name m_fname{ "engine.log" };
+
+    //
+    // Buffer for formatted messages
+    //
     inline static buf_type  m_buf;
 
+    //
+    // Underlying log buffer where messages are accumulated
+    //
     inline static std::stringstream m_stream;
+
+    //
+    // Log file currently written to
+    //
     inline static std::ofstream m_file;
 
+    //
+    // Current logging level
+    //
     inline static level m_lvl{
     #ifndef NDEBUG
       dbg
--- neko/include/managers/sys_registry.hpp
+++ neko/include/managers/sys_registry.hpp
@@ -1,3 +1,7 @@
+//
+// Core systems manager
+//
+
 #pragma once
 #include "core/singleton_base.hpp"
 #include "managers/app_host.hpp"
@@ -14,22 +18,36 @@ namespace neko
   class app_host;
   class conf_manager;
 
+
+  //
+  // Core manager for all engine systems
+  // Handles startup and shutdown, and provides access to specific systems
+  //
   class systems final
   {
   public:
     CLASS_SPECIALS_NONE(systems);
 
   public:
+    //
+    // Returns a reference to the applcation host
+    //
     static decltype(auto) app_host() noexcept
     {
       return singleton<neko::app_host>::get();
     }
 
+    //
+    // Returns a reference to the config manager
+    //
     static decltype(auto) config() noexcept
     {
       return singleton<neko::conf_manager>::get();
     }
 
+    //
+    // Checks if a system exists
+    //
     template <detail::engine_system Sys>
     static bool good() noexcept
     {
@@ -39,6 +57,14 @@ namespace neko
   private:
     friend class core;
 
+    //
+    // Creates a system from a set of parameters
+    // The Derived template param specifies which derived class to instantiate
+    // 
+    // This is handy for platform-specific stuff
+    // For example, window inherits from app_host, which is an abstract class
+    // We can create a window instance: init_system<app_host, window>(...)
+    //
     template <detail::engine_system Sys, typename Derived, typename ...Args>
       requires (std::is_base_of_v<Sys, Derived>)
     static bool init_system(Args&& ...args) noexcept
@@ -46,12 +72,18 @@ namespace neko
       return singleton<Sys>::template create<Derived>(std::forward<Args>(args)...);
     }
 
+    //
+    // Creates a system from a set of parameters
+    //
     template <detail::engine_system Sys, typename ...Args>
     static bool init_system(Args&& ...args) noexcept
     {
       return init_system<Sys, Sys>(std::forward<Args>(args)...);
     }
 
+    //
+    // Shuts the specified system down
+    //
     template <detail::engine_system Sys>
     static void shutdown_system() noexcept
     {
